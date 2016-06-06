9066ac2076c46732414da9379c04c3825ef0a7e9
Matt Diamond <matt@publicstuff.com>
2016-06-06 17:52:07 -0400

    switch to config var for threshold

--- ouija.js
+++ ouija.js
@@ -19,7 +19,7 @@ if (process.env.client_id){
 
 const
 	OUIJA_RESULT_CLASS = 'ouija-result',
-	COMMENT_SCORE_THRESHOLD = 5;
+	COMMENT_SCORE_THRESHOLD = process.env.threshold;
 
 var
 	r = new snoowrap(config),
